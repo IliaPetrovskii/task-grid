9511ca185fa25e24c240b4099e42551012ced825
IliaPetrovskii <80913527+IliaPetrovskii@users.noreply.github.com>
2022-10-06 15:20:10 +0300

    Initial commit

---
+++ src/App.test.ts
@@ -0,0 +1,43 @@
+import { configureToMatchImageSnapshot } from 'jest-image-snapshot';
+import puppeteer, { Page, Browser } from 'puppeteer';
+import path from 'path';
+
+const customConfig = { threshold: 0 };
+const toMatchImageSnapshot = configureToMatchImageSnapshot({
+    customDiffConfig: customConfig,
+    noColors: true,
+    failureThreshold: 0.08,
+    failureThresholdType: 'percent'
+});
+
+let browser: Browser;
+
+beforeAll(async () => {
+    browser = await puppeteer.launch();
+});
+
+afterAll(async () => {
+    await browser.close();
+});
+
+expect.extend({ toMatchImageSnapshot });
+
+describe('Grid', () => {
+    let page: Page;
+    beforeEach(async () => {
+        page = await browser.newPage();
+        await page.goto(`file:${path.join(__dirname, 'index.html')}`);
+    });
+
+    it('Верстка не тронута', async () => {
+        const container = await page.evaluate(() => document.body.innerHTML);
+
+        expect(container).toMatchSnapshot();
+    });
+
+    it('Рисуется нужное изображение', async () => {
+        const image = await page.screenshot();
+
+        expect(image).toMatchImageSnapshot();
+    });
+});
